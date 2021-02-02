96c9201d51537f705f1c2e08eb680365278ff708
jjenkins120 <jjenkins120@gmail.com>
2021-02-02 16:42:54 -0600

    add a custom route for the portfolio item

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  #IF YOU WANTED TO MAKE A CUSTOM ROUTE WITH RESOURCES:
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   resources :blogs
 
   get 'about-me', to: 'pages#about'
