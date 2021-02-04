1af22ed669c247a4863ccf02ccf5c5807f903153
jjenkins120 <jjenkins120@gmail.com>
2021-02-04 10:57:05 -0600

    implemented basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   #IF YOU WANTED TO MAKE A CUSTOM ROUTE WITH RESOURCES:
   # resources :portfolios, except: [:show]
   # get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
